c8261ee37c14ca8047a3ef1d1258cbb86bd58766
Skyphoenix <jan@vandenhouten.de>
2025-01-19 16:38:12 +0100

    Added subnets

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,18 @@ variable "vpc_cidr" {
   type        = string
   description = "Public Subnet CIDR values"
 }
+
+variable "cidr_public_subnet" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+}
+
+variable "cidr_private_subnet" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+}
+
+variable "us_availability_zone" {
+  type        = list(string)
+  description = "Availability Zones"
+}

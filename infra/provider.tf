c0fc097a7aeb617269f710d8b79b01b8bf944220
Skyphoenix <jan@vandenhouten.de>
2025-01-20 08:42:39 +0100

    updated CI/CD

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket"
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"        # (Optional) DynamoDB table for state locking
+    encrypt        = true                          # Enable state file encryption
+  }
+}
+
+
 provider "aws" {
   region                   = "us-east-1"
   # shared_credentials_files = ["/Users/janvandenhouten/.aws/credentials"]
